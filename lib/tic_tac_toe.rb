81594b67f98ad3c66edf92e7a9b3af18ea340808
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 16:21:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ class TicTacToe
         position2 = @board[winArr[1]]
         position3 = @board[winArr[2]]
         
-        if postion1 == position2 && 
+        if postion1 == position2 && position1 == position3
         
     end
   end
